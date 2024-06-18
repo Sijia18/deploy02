73cc7da0e43829d58ad385a6637854f0df5c76c3
Aurora Scalici <aurorascalici1899@icloud.com>
2024-06-18 11:42:04 +0200

    fix exercise

--- src/main/java/co/develhope/deploy/OperationController.java
+++ src/main/java/co/develhope/deploy/OperationController.java
@@ -1,13 +1,20 @@
 package co.develhope.deploy;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OperationController {
 
-    @GetMapping("/sum")
-    public int sum(int a, int b){
-        return a + b;
+    @Value("${int1}")
+    private int int1;
+
+    @Value("${int2}")
+    private int int2;
+
+    @GetMapping
+    public int sum() {
+        return int1 + int2;
     }
 }
